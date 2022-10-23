760a34656e3575d6e60cd828ea49c0b97d32f89b
Mikkel Blomnes <mikkel.blomnes@gmail.com>
2022-10-23 09:30:07 +0200

    added clock

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from "./logo.svg";
 import ShowerThoughts from "./ShowerThoughts";
 import Buses from "./Buses";
+import Clock from "./Clock";
 
 function App() {
   return (
     <div className="App">
+      <Clock />
       <ShowerThoughts />
       <Buses />
     </div>
